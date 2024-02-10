0d47ba2a00b70dc6e99ddf4d7c85632558a3efaf
yasuo kurihara <yasuo-@users.noreply.github.com>
2024-02-10 19:09:46 +0900

    feat(schema): Update Task schema and related adjustments

--- app/routers/done.py
+++ app/routers/done.py
@@ -3,11 +3,11 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.put("/tasks/{task_id}/done")
-async def mark_task_as_done(task_id: int) -> dict:
-    pass
+@router.put("/tasks/{task_id}/done", response_model=None)
+async def mark_task_as_done(task_id: int) -> None:
+    return None
 
 
-@router.delete("/tasks/{task_id}/done")
-async def mark_task_as_not_done(task_id: int) -> dict:
-    pass
+@router.delete("/tasks/{task_id}/done", response_model=None)
+async def mark_task_as_not_done(task_id: int) -> None:
+    return None
--- app/routers/task.py
+++ app/routers/task.py
@@ -1,28 +1,32 @@
 from fastapi import APIRouter
+import app.schemas.task as tasks_schema
 
 router = APIRouter()
 
 
-@router.get("/tasks")
-async def list_tasks() -> dict:
-    pass
+@router.get("/tasks", response_model=list[tasks_schema.Task])
+async def list_tasks() -> list[tasks_schema.Task]:
+    return [
+        tasks_schema.Task(id=1, title="Task 1", done=False),
+        tasks_schema.Task(id=2, title="Task 2", done=True),
+    ]
 
 
-@router.post("/tasks")
-async def create_task() -> dict:
-    pass
+@router.post("/tasks", response_model=tasks_schema.TaskCreateResponse)
+async def create_task(body: tasks_schema.TaskCreate) -> tasks_schema.TaskCreateResponse:
+    return tasks_schema.TaskCreateResponse(id=1, title=body.title, done=False)
 
 
-@router.get("/tasks/{task_id}")
-async def get_task(task_id: int) -> dict:
-    pass
+@router.get("/tasks/{task_id}", response_model=tasks_schema.Task)
+async def get_task(task_id: int) -> tasks_schema.Task:
+    return tasks_schema.Task(id=task_id, title="Task 1", done=False)
 
 
-@router.put("/tasks/{task_id}")
-async def update_task(task_id: int) -> dict:
-    pass
+@router.put("/tasks/{task_id}", response_model=tasks_schema.TaskCreateResponse)
+async def update_task(task_id: int, body: tasks_schema.TaskCreate) -> tasks_schema.TaskCreateResponse:
+    return tasks_schema.TaskCreateResponse(id=task_id, title=body.title, done=False)
 
 
-@router.delete("/tasks/{task_id}")
-async def delete_task(task_id: int) -> dict:
-    pass
+@router.delete("/tasks/{task_id}", response_model=None)
+async def delete_task(task_id: int) -> None:
+    return None
