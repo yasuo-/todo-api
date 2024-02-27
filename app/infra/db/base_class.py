0846e8000f057194bebeb20147e324859f466d6f
yasuo kurihara <yasuo-@users.noreply.github.com>
2024-02-27 21:03:22 +0900

    fix: bug fix migration

--- app/infra/db/base_class.py
+++ app/infra/db/base_class.py
@@ -1,22 +1,18 @@
-import datetime
-from uuid import UUID, uuid4
+from datetime import datetime
 
-from sqlalchemy import Column
-from sqlalchemy.dialects.postgresql import UUID as pgUUID  # noqa: N811
 from sqlmodel import Field, SQLModel, text
 
 
 class BaseModel(SQLModel):
-    id: UUID = Field(default_factory=uuid4, sa_column=Column(pgUUID(as_uuid=True), primary_key=True))
     created_at: datetime = Field(
+        default_factory=datetime.utcnow,
         nullable=False,
         sa_column_kwargs={"server_default": text("CURRENT_TIMESTAMP")},
         title="Date Created",
     )
     updated_at: datetime = Field(
+        default_factory=datetime.utcnow,
         nullable=False,
-        sa_column_kwargs={
-            "server_default": text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
-        },
+        sa_column_kwargs={"server_default": text("CURRENT_TIMESTAMP"), "onupdate": datetime.now},
         title="Date Updated",
     )
--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,7 @@
+from uuid import UUID, uuid4
+
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import UUID as pgUUID  # noqa: N811
 from sqlmodel import Field
 
 from app.infra.db.base_class import BaseModel
@@ -5,4 +9,5 @@ from app.infra.db.base_class import BaseModel
 
 class User(BaseModel, table=True):
     __tablename__ = "users"
+    id: UUID = Field(default_factory=uuid4, sa_column=Column(pgUUID(as_uuid=True), primary_key=True))
     name: str = Field(max_length=50)
